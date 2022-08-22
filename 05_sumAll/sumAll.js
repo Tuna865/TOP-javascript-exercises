87f87a739a845d23f20b8f924265486eedf98140
Turner Milan <turnermilan@gmail.com>
2022-08-21 22:01:56 -0400

    completed exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if(x < 0 || y < 0) 
+        return "ERROR"
+    if (!Number.isInteger(x) || !Number.isInteger(y)) 
+        return "ERROR"
+        
+    const lowNum = Math.min(x,y)
+    const highNum = Math.max(x,y)
+    
+    let sum = highNum * (highNum + 1) / 2 - (lowNum - 1) * (lowNum) / 2
+    return sum
     
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
 
+/*
+
+°F = °C × 9⁄5 + 32
+°C = (°F − 32) × 5⁄9
+*/ 
+
+const ftoc = function(temp) {
+  let fahrenheit = (temp - 32) * (5/9)
+  
+  return Math.round(fahrenheit * 10) / 10
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  celsius = temp * (9/5) + 32
 
+  return Math.round(celsius * 10) / 10
 };
 
 // Do not edit below this line
