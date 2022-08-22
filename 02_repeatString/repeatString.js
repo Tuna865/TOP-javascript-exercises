e0c2f888609738f48d6e989231f696636208d5e6
Turner Milan <turnermilan@gmail.com>
2022-08-21 20:24:47 -0400

    completed exercises 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+let example = 'hey'
 
+const repeatString = function(string, num) {
+    if(num >=0){
+        return string.repeat(num)
+    } else {
+        return 'ERROR'
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
